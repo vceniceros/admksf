827b90369f1bddca2706ef69078d42469b1600cb
vceniceros <vceniceros2001@gmail.com>
2026-01-01 14:16:11 -0300

    fix: se intenta resolver el 400 bad request

--- backend/backend_core/settings.py
+++ backend/backend_core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l=vlgp=lql^v&ivl%%*g1@z5_!x!-w^ynv*agp6!2udmc(=-31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'backend_core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/backend_core/urls.py
+++ backend/backend_core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, re_path          
+from django.views.generic import TemplateView  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
