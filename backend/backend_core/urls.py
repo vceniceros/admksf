0f73d806d0b84744aee22483c54f04ac607b8e14
vceniceros <vceniceros2001@gmail.com>
2026-01-01 14:24:33 -0300

    fix: se logra mostrar el frontend

--- backend/backend_core/urls.py
+++ backend/backend_core/urls.py
@@ -19,7 +19,9 @@ from django.urls import path
 from django.urls import path, re_path          
 from django.views.generic import TemplateView  
 
+from django.views.generic.base import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html'), name='frontend'),
 ]
